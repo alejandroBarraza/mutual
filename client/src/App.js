2100b98cf0b6c7042876456599d79215c090b236
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-09-23 13:54:38 -0300

    apollo client configuration, and graphql-client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import { Navbar } from './components/home/navbar/Navbar';
+import { ApolloClient, InMemoryCache, ApolloProvider } from 'apollo-client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:1337/graphql',
+    cache: new InMemoryCache(),
+});
 
 function App() {
     return (
         <div>
-            <Navbar />
+            <ApolloProvider client={client}>
+                <Navbar />
+            </ApolloProvider>
         </div>
     );
 }
